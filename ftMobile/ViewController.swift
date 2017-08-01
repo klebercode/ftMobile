5b309d6c24de4badea1f4a6cd49169223417f914
toanhtran <toanh.t.tran@gmail.com>
2017-08-01 16:26:53 -0700

    Initial Commit

---
+++ ftMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ftMobile
+//
+//  Created by ToAnh T. Tran on 8/1/17.
+//  Copyright © 2017 ToAnh T. Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
